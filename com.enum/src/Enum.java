db5ac232512c112da41d9cd854c8bce077e24a14
44742 <44742@ProgramminHelp>
2019-05-12 19:48:24 +0100

    commit enum java

---
+++ com.enum/src/Enum.java
@@ -0,0 +1,14 @@
+/**
+ * @author msfata ProgrammingHelp
+ *
+ */
+enum Level {
+	LOW, MEDIUM, HIGH
+}
+
+public class Enum {
+	public static void main(String[] args) {
+		Level myVar = Level.MEDIUM;
+		System.out.println(myVar);
+	}
+}
